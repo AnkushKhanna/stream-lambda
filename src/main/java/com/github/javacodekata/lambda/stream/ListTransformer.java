29889b80b27ef90412b4763f9c7119dc3eea877b
Ankush Khanna <ankushkhanna@customer-dynamic-217-115-9-132.inaddr.berlinwireless.de>
2015-04-06 21:05:23 +0200

    Learning Java 8 lambda expressions.

--- src/main/java/com/github/javacodekata/lambda/stream/ListTransformer.java
+++ src/main/java/com/github/javacodekata/lambda/stream/ListTransformer.java
@@ -2,8 +2,10 @@ package com.github.javacodekata.lambda.stream;
 
 import com.google.common.collect.ImmutableList;
 
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -22,36 +24,48 @@ public class ListTransformer {
 
 	/**
 	 * This method should take the String List and sort all the String elements in ascending (ASCII) order.
+	 * 
 	 * @return The sorted values in ascending ASCII order.
 	 */
 	public List<String> getSortedStrings() {
-		return values;
+		return values.stream().sorted().collect(Collectors.toList());
 	}
 
 	/**
 	 * This method should take the String List and:
 	 * <ol>
-	 *     <li>filter the elements that contains one or more digits;</li>
-	 *     <li>transform (map) the remaining Strings into Integers;</li>
-	 *     <li>sort the Integers in ascending order.</li>
+	 * <li>filter the elements that contains one or more digits;</li>
+	 * <li>transform (map) the remaining Strings into Integers;</li>
+	 * <li>sort the Integers in ascending order.</li>
 	 * </ol>
+	 * 
 	 * @return
 	 */
 	public List<Integer> getSortedIntegers() {
-		return new LinkedList<>();
+		return values.stream().filter(this::isInteger).mapToInt(Integer::parseInt).sorted().boxed().collect(Collectors.toList());
 	}
 
 	/**
 	 * This method should take the String List and:
 	 * <ol>
-	 *     <li>filter the elements that contains one or more digits;</li>
-	 *     <li>transform (map) the remaining Strings into Integers;</li>
-	 *     <li>sort the Integers in descending order.</li>
+	 * <li>filter the elements that contains one or more digits;</li>
+	 * <li>transform (map) the remaining Strings into Integers;</li>
+	 * <li>sort the Integers in descending order.</li>
 	 * </ol>
+	 * 
 	 * @return
 	 */
 	public List<Integer> getSortedDescendingIntegers() {
-		return new LinkedList<>();
+		return values.stream().filter(this::isInteger).map(Integer::parseInt).sorted((a,b)->b.compareTo(a))
+				.collect(Collectors.toList());
 	}
 
+	private boolean isInteger(String x) {
+		try {
+			Integer.parseInt(x);
+			return true;
+		} catch (NumberFormatException ex) {
+			return false;
+		}
+	}
 }
